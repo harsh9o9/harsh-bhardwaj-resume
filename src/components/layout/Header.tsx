2fc5ae57f75f35912ee69f8ada578db1ef3cc110
harsh9o9 <hbhardwaj454@gmail.com>
2025-09-26 01:00:56 +0530

    Removed heavy bastiliga font file and replaced it with an optimized image

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,13 +1,17 @@
 import Link from 'next/link';
 import HoverUnderline from '../ui/HoverUnderline';
+import Image from 'next/image';
 
 export default function Header(): React.JSX.Element {
   return (
     <header className="fixed inset-x-0 top-0 z-10 flex min-h-fit justify-between bg-transparent px-2 sm:px-4">
-      <p className="font-bastliga ml-1 flex -rotate-[30deg] flex-col gap-1 p-2 text-xl text-white sm:ml-3 sm:text-2xl lg:text-3xl">
-        <span>Harsh</span>
-        <span>Bhardwaj</span>
-      </p>
+      <Image
+        src="/images/harsh_bhardwaj.avif"
+        alt="Harsh Bhardwaj"
+        width={100}
+        height={100}
+        loading="lazy"
+      />
       <div className="hidden sm:block">
         <div className="font-technology bg-neutral-500/90 px-6 py-4 text-lg text-white italic sm:px-8 sm:py-5 sm:text-xl lg:px-11 lg:py-6">
           <HoverUnderline underlineColor="bg-neutral-500/90">
